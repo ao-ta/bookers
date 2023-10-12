161f55681696e1c1ddf366b09fd90ae6312db3e7
EC2 Default User <ec2-user@ip-172-31-46-157.ap-northeast-1.compute.internal>
2023-10-12 05:56:02 +0000

    [Finish-8] 1012

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,22 +5,41 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/'
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :new
+    end
   end
+
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+     book = Book.find(params[:id]) #データを1件取得
+    book.destroy #データを削除
+    redirect_to '/books'
   end
 
   private
   #ストロングパラメータ
   def book_params
     params.require(:book).permit(:title, :body)
-end
+  end
 end
